da634210f2b340327cff401f0d0f8561e9b962f4
zpdelong <zacherypd@gmail.com>
2019-03-27 21:39:45 -0400

    Reading not working yet
    Implemented basic parsing, but for some reason everything is going into the
    one element. I think it might be related to the format of the XML doc

--- prototype_reader.py
+++ prototype_reader.py
@@ -1,10 +1,21 @@
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
 import requests
+
 nyt_rss = "https://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
 
 r = requests.get(nyt_rss, verify = False)
 
-print(r.text)
+lines = r.text.splitlines()
 
+lines = lines[1:]
 
+with open("output.txt", "w") as file:
+    file.write("\n".join(lines))
 
+xml_doc = ET.parse("\n".join(lines))
+
+root = xml_doc.getroot()
+
+tag = len(root)
+
+print(tag)
