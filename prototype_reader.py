8139c735a19de54f5d25facab89c0979d1977d16
zpdelong <zacherypd@gmail.com>
2019-03-28 18:25:14 -0400

    Fixing parsing to find articles in the sample feed

--- prototype_reader.py
+++ prototype_reader.py
@@ -5,17 +5,8 @@ nyt_rss = "https://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
 
 r = requests.get(nyt_rss, verify = False)
 
-lines = r.text.splitlines()
+xml_doc = ET.fromstring(r.text)
 
-lines = lines[1:]
+tags = xml_doc.findall('channel/item')
 
-with open("output.txt", "w") as file:
-    file.write("\n".join(lines))
-
-xml_doc = ET.parse("\n".join(lines))
-
-root = xml_doc.getroot()
-
-tag = len(root)
-
-print(tag)
+print(len(tags))
